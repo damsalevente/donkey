9d149b029fa2e0bc41627291d804671737d52d16
Levente Dámsa <damsalevente@gmail.com>
2020-12-01 23:20:21 +0100

    Successful migration

---
+++ WeCanCSharp/DonkeyDBMigrations/Migrations/20201201221831_Db-Create.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DonkeyDBMigrations.Migrations
+{
+    public partial class DbCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Donkeys",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Angle = table.Column<double>(type: "REAL", nullable: false),
+                    Throttle = table.Column<double>(type: "REAL", nullable: false),
+                    TimeStamp = table.Column<ulong>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Donkeys", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Donkeys");
+        }
+    }
+}
