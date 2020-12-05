d114a653ecd4eaefa802e73facfc2e39914886d6
Tóth Mátyás <62833377+mattyer282@users.noreply.github.com>
2020-12-05 16:35:51 +0100

    Add files via upload

---
+++ WeCanCSharp/WeCanCSharp/DAL/DonkeyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeCanCSharp.Model;
+
+namespace WeCanCSharp.DAL
+{
+    class DonkeyContext:DbContext
+    {
+        public DbSet<ModelDonkeyData> Donkeys { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder modelBuilder)
+        {
+            modelBuilder.UseSqlite("Data Source = donkey.db")
+        }
+    }
+
+}
