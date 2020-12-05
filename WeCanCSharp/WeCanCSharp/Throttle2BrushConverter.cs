f99b08afb71299579cb4df18976e3f1f0bfe1684
Tóth Mátyás <62833377+mattyer282@users.noreply.github.com>
2020-12-05 21:04:23 +0100

    updated converter

--- WeCanCSharp/WeCanCSharp/Throttle2BrushConverter.cs
+++ WeCanCSharp/WeCanCSharp/Throttle2BrushConverter.cs
@@ -11,42 +11,13 @@ namespace WeCanCSharp
 {
     class Throttle2BrushConverter : IValueConverter
     {
-        readonly private static SolidColorBrush blue = new SolidColorBrush(Colors.Blue);
-        readonly private static SolidColorBrush dblue = new SolidColorBrush(Colors.DodgerBlue);
-        readonly private static SolidColorBrush dsblue = new SolidColorBrush(Colors.DeepSkyBlue);
-        readonly private static SolidColorBrush lsblue = new SolidColorBrush(Colors.LightSkyBlue);
-        readonly private static SolidColorBrush pblue = new SolidColorBrush(Colors.PowderBlue);
-        readonly private static SolidColorBrush black = new SolidColorBrush(Colors.Black);
-        readonly private static SolidColorBrush maroon = new SolidColorBrush(Colors.Maroon);
-        readonly private static SolidColorBrush dred = new SolidColorBrush(Colors.DarkRed);
-        readonly private static SolidColorBrush fb = new SolidColorBrush(Colors.Firebrick);
-        readonly private static SolidColorBrush red = new SolidColorBrush(Colors.Red);
-        readonly private static SolidColorBrush crimson = new SolidColorBrush(Colors.Crimson);
-        readonly private static SolidColorBrush[] brushes = new[] { blue, dblue, dsblue, lsblue, pblue, maroon, dred, fb, red, crimson };
-
+        readonly private static byte[] brushes = { 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+        SolidColorBrush brush; 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if ((int)value == 0)
-            {
-                return black;
-            }
-            if ((int)value == 10)
-            {
-                return brushes[9];
-            }
-            else
-            {
-                if (((int)value) % 2 == 0)
-                {
-                    return brushes[((int)value / 2) + 5];
-                }
-                else
-                {
-                    return brushes[(((int)value - 1) / 2) + 5];
-                }
-            }
+            brush = new SolidColorBrush(Color.FromArgb(brushes[(int)value], 0xff, 0x00, 0x00));
+            return brush;
         }
-
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
