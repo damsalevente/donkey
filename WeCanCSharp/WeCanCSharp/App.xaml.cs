772c2e5645eea982e0c5b42589e8213a563b48cf
Levente Dámsa <damsalevente@gmail.com>
2020-12-06 02:21:35 +0100

    Throttle2Brush update

--- WeCanCSharp/WeCanCSharp/App.xaml.cs
+++ WeCanCSharp/WeCanCSharp/App.xaml.cs
@@ -151,7 +151,7 @@ namespace WeCanCSharp
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
+            
             deferral.Complete();
         }
     }
--- WeCanCSharp/WeCanCSharp/Throttle2BrushConverter.cs
+++ WeCanCSharp/WeCanCSharp/Throttle2BrushConverter.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
@@ -11,11 +7,13 @@ namespace WeCanCSharp
 {
     class Throttle2BrushConverter : IValueConverter
     {
-        readonly private static byte[] brushes = { 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
-        SolidColorBrush brush; 
+        readonly private static byte[] brushes = { 55, 65, 75, 85, 95, 105, 115, 125, 135, 145, 155, 165, 175, 185, 195, 205, 215, 225, 235, 245, 255 };
+        SolidColorBrush brush;
+        int value2;
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            brush = new SolidColorBrush(Color.FromArgb(brushes[(int)value], 0xff, 0x00, 0x00));
+            value2 = System.Convert.ToInt32(value) + 10;
+            brush = new SolidColorBrush(Color.FromArgb(brushes[value2], 0, 255, 0));
             return brush;
         }
         public object ConvertBack(object value, Type targetType, object parameter, string language)
